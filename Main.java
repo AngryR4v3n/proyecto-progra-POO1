479653b9a62f57c5273309eecd3cbdd3b1e5db5f
diazMafer <31084398+diazMafer@users.noreply.github.com>
2017-08-17 23:19:37 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+/**
+    * Interactua con el usuario
+    * 
+    
+    @author María Fernanda López, Francisco Molina, Paolo Suchini 
+    @version 11/08/2017
+    
+*/
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main{
+	 public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido a meet 502");
+        System.out.println("1.Iniciar sesion"+"\n2.Menu");
+        int opcion = sc.nextInt();
+           
+        
+    }
+}
