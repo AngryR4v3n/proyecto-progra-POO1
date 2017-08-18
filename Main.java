646e4724ebf1b272a150f205421e2e4a27342449
diazMafer <31084398+diazMafer@users.noreply.github.com>
2017-08-18 12:56:13 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -13,9 +13,64 @@ import java.util.ArrayList;
 public class Main{
 	 public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        Departamento depa = new Departamento();
+        Restaurante restau = new Restaurante();
+        Hotel hotel = new Hotel();
+        Evento evento = new Evento();
+        HQ opera = new HQ();
         System.out.println("Bienvenido a meet 502");
-        System.out.println("1.Iniciar sesion"+"\n2.Menu");
+        System.out.println("1.Administrador"+"\n2.Usuario");
         int opcion = sc.nextInt();
+        if (opcion==1){
+            Sysytem.out.println("1. Agregar hotel" + "\n2. Agregar Restaurante" + "\n3. Agregar evento" + "\n4. Agregar Departamento");
+            int select = sc.nextInte();
+            if (select==1){
+                System.out.println("Ingrese el nombre del hotel");
+                String nombre = sc.next();
+                System.out.println("Ingresa la direccion");
+                String direccion ? sc.next();
+                System.out.println("Ingrese el correo");
+                String correo = sc.next();
+                System.out.println("Ingrese el numero de telefono");
+                String telefono = sc.next();
+                System.out.println("Ingrese la descripcio");
+                String descripcion = sc.next();
+                System.out.println("Ingrese el horario");
+                String horario = sc.next();
+                System.out.println("Ingrese la calificacion");
+                double calificacion = sc.nextDouble();
+
+                opera.addH(nombre, direccion, correo, telefono, descripcion, horario, calificacion);
+           }
+
+           if(select==2){
+                System.out.println("Ingrese el nombre del restaurante");
+                String nombre = sc.next();
+                System.out.println("Ingresa la direccion");
+                String direccion ? sc.next();
+                System.out.println("Ingrese el horario");
+                String horario = sc.next();
+                System.out.println("Ingrese la calificacion");
+                double calificacion = sc.nextDouble();
+                System.out.println("Ingrese el numero de telefono");
+                String telefono = sc.next();
+
+                opera.addRes(nombre, direccion, horario, calificacion, telefono);
+
+           }
+
+           if(select==3){
+                System.out.println("Ingrese el nombre del restaurante");
+                String nombre = sc.next();
+                System.out.println("Ingresa la direccion");
+                String direccion = sc.next();
+                opera.add
+
+
+           }
+
+
+        }
            
         
     }
