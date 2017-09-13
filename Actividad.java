ced601eac9095220674ff3c65c56f4d4c97c8620
diazMafer <31084398+diazMafer@users.noreply.github.com>
2017-09-13 14:18:50 -0600

    Update Actividad.java

--- Actividad.java
+++ Actividad.java
@@ -13,13 +13,15 @@ public class Actividad{
 	private String tipo;
 	private String horario;
 	private String direccion; 
+	private String departamento;
 
 	//Constructor
-	public Actividad(String nombre, String tipo, String horario, String direccion){
+	public Actividad(String nombre, String tipo, String horario, String direccion, String departamento){
 		this.nombre=nombre;
 		this.tipo=tipo;
 		this.horario=horario;
 		this.direccion=direccion;
+		this.departamento=departamento;
 	}
 
 	//gets
@@ -33,5 +35,9 @@ public class Actividad{
 
 	public String getNombre(){
 		return nombre;
+	}
+	
+	public String getDepa(){
+		return departamento;
 	}
 }
